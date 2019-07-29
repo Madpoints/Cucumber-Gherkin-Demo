06fa4880c0dd1fce2f86236d44d1db30482eb53c
John Furtado <john.furtado@virginpulse.com>
2019-07-29 15:36:55 -0400

    glue code and step definitions

---
+++ src/test/java/com/pluralsight/bdd/loyalty_cards/DrinkCatalog.java
@@ -0,0 +1,13 @@
+package com.pluralsight.bdd.loyalty_cards;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DrinkCatalog {
+
+    Map<String, String> drinkCategories = new HashMap<>();
+
+    public void addDrink(String drink, String category) {
+        drinkCategories.put(drink, category);
+    }
+}
---
+++ src/test/java/com/pluralsight/bdd/loyalty_cards/SuperSmoothieSchema.java
@@ -0,0 +1,12 @@
+package com.pluralsight.bdd.loyalty_cards;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SuperSmoothieSchema {
+    private Map<String, Integer> pointsPerCategory = new HashMap<>();
+
+    public void setPointsPerCategory(String category, Integer points) {
+        pointsPerCategory.put(category, points);
+    }
+}
